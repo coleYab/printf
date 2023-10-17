df22b3dd38164cf04bd3edce49050b92f2c9cc6d
Farida Al Shamaa <140178428+faridaalshamaa@users.noreply.github.com>
2023-10-17 22:34:56 +0300

    Update num.c

--- num.c
+++ num.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * dec_printer - prints the number in base 10 as ifi.
+ * dec_printer - prints the number in base 10 as i.
  *
  * @va: the variadic arg list.
  * @buffer_storge: the buffer to string.
