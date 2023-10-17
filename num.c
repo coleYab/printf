3c9d179be436630009264a86c122ca0d45611907
Farida Al Shamaa <140178428+faridaalshamaa@users.noreply.github.com>
2023-10-17 22:19:25 +0300

    Update num.c

--- num.c
+++ num.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * dec_printer - prints the number in any base.
+ * dec_printer - prints the number in .
  *
  * @va: the variadic arg list.
  * @buffer_storge: the buffer to store chars.
